bb3bb39344f51c6693ec54370a18c53c8e177117
Maxim <shuteyshiy41@gmail.com>
2017-01-29 12:10:35 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Администратор on 30.10.2016.
+ */
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 3200));
+
+app.use('/', express.static(__dirname));
+
+app.listen(app.get('port'), function() {
+    console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
